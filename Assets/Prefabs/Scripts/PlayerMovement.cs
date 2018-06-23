ed9cc253d488c16f2a9cee6dae4ab4c01d1008b1
Alex Liu <34083577+Naranja-Sagged@users.noreply.github.com>
2018-06-23 08:42:24 -0700

    Added isGrounded to limit jumping but it wont work wth

--- Assets/Prefabs/Scripts/PlayerMovement.cs
+++ Assets/Prefabs/Scripts/PlayerMovement.cs
@@ -6,19 +6,28 @@ public class PlayerMovement : MonoBehaviour {
     public float speed;
     float speedDelta = 150;
     GameObject player;
+    CapsuleCollider2D playerCollider;
 	// Use this for initialization
 	void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
-	}
+        playerCollider = player.GetComponent<CapsuleCollider2D>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey(KeyCode.D)) {
-            player.transform.position = (Vector2)player.transform.position + Vector2.right * (speed/speedDelta);
+            player.transform.position = (Vector2)player.transform.position + Vector2.right * (speed / speedDelta);
         }
-        if (Input.GetKey(KeyCode.A))
-        {
-            player.transform.position = (Vector2)player.transform.position + Vector2.left * (speed/speedDelta);
+        if (Input.GetKey(KeyCode.A)) { 
+            player.transform.position = (Vector2)player.transform.position + Vector2.left * (speed / speedDelta);
+        }
+        if (Input.GetKey(KeyCode.Space)) {
+            float distFromGround = playerCollider.bounds.extents.y;
+            bool isGrounded = Physics.Raycast(transform.position, Vector3.down, distFromGround + 1.0f);
+            if (!isGrounded) {
+                player.transform.position = (Vector2)player.transform.position + Vector2.up * ((speed + 1) / speedDelta);
+            }
         }
     }
 }
