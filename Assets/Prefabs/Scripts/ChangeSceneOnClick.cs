688ba41181aad0baa01c488e9510e099200bfd5e
Alex Liu <34083577+Naranja-Sagged@users.noreply.github.com>
2018-06-23 02:19:37 -0700

    Title: Added button objects

---
+++ Assets/Prefabs/Scripts/ChangeSceneOnClick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeSceneOnClick : MonoBehaviour {
+    public string sceneName = "CharacterSelection";
+    void Start() {
+        GameObject gaymeObject = GameObject.Find("Play");
+        Button button = gaymeObject.GetComponent<Button>();
+        button.onClick.AddListener(TaskOnClick);
+    }
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
