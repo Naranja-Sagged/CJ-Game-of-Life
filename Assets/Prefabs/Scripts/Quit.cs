a2cc45cab1bfdbedb7655c2f844a2505a0b72e33
Alex Liu <34083577+Naranja-Sagged@users.noreply.github.com>
2018-06-23 03:23:12 -0700

    Quit button works with clicks now

--- Assets/Prefabs/Scripts/Quit.cs
+++ Assets/Prefabs/Scripts/Quit.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Quit : MonoBehaviour {
-    public void OnApplicationQuit(){
+    //Gets button name from inspector's text
+    public string buttonName;
+    void Start()
+    {
+        GameObject gaymeObject = GameObject.Find(buttonName);
+        Button button = gaymeObject.GetComponent<Button>();
+
+        button.onClick.AddListener(TaskOnClick);
+    }
+    void TaskOnClick() //Quits game
+    {
         UnityEditor.EditorApplication.isPlaying = false;
         Application.Quit();
-        Console.WriteLine("Quit is working");
     }
 }
