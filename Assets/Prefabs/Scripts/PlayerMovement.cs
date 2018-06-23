031b4925da3e4e8fa566e9e9bd6d555d518492e2
Alex Liu <34083577+Naranja-Sagged@users.noreply.github.com>
2018-06-23 05:14:14 -0700

    PlayerMovement: A and D movements added

---
+++ Assets/Prefabs/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public GameObject player;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.D)) {
+            player.transform.position = (Vector2)player.transform.position + Vector2.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.transform.position = (Vector2)player.transform.position + Vector2.left;
+        }
+    }
+}
