b5eac43865eab8d5aef255de00233a5da47b5bf5
Alex Liu <34083577+Naranja-Sagged@users.noreply.github.com>
2018-06-23 02:39:14 -0700

    test

--- Assets/Prefabs/Scripts/ChangeSceneOnClick.cs
+++ Assets/Prefabs/Scripts/ChangeSceneOnClick.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ChangeSceneOnClick : MonoBehaviour {
+    //String names defined in inspector
     public string sceneName;
     public string buttonName;
     void Start() {
         GameObject gaymeObject = GameObject.Find(buttonName);
         Button button = gaymeObject.GetComponent<Button>();
+        //Makes button interactable
         button.onClick.AddListener(TaskOnClick);
     }
     void TaskOnClick()
