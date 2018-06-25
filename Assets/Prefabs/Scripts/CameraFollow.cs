00453d2dd8b9eea61b770a7fe0731aad79b72509
Alex Liu <34083577+Naranja-Sagged@users.noreply.github.com>
2018-06-25 06:30:37 -0700

    Camera: Script added, camera now followers player

---
+++ Assets/Prefabs/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    Transform playerTransform;
+    Vector3 initCameraPos;
+
+	// Use this for initialization
+	void Start () {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        initCameraPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = playerTransform.position + initCameraPos;
+	}
+}
