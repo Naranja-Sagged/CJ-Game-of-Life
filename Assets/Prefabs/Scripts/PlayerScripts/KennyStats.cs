6b786afa77b3724648efb2b221ca41679fb57653
Kelvin Chan <33172485+MyAngelNeptune@users.noreply.github.com>
2018-06-25 04:49:56 -0700

    Kenny stats
    Stats

---
+++ Assets/Prefabs/Scripts/PlayerScripts/KennyStats.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KennyStats : MonoBehaviour {
+    public float health;
+    public float mana;
+    public float attack;
+    public float defense;
+
+}
